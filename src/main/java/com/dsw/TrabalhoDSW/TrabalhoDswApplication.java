56fa579635baac81ca17793936b0218685892404
Lucas Castro <luukascastro@gmail.com>
2021-07-05 13:57:23 -0300

    Projeto criado

---
+++ src/main/java/com/dsw/TrabalhoDSW/TrabalhoDswApplication.java
@@ -0,0 +1,13 @@
+package com.dsw.TrabalhoDSW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsw/TrabalhoDSW/TrabalhoDswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsw.TrabalhoDSW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoDswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
